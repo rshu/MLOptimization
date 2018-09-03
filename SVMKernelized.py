6e8f6e98713c88b691783561d1df9f38432fc740
Rui Shu <terry.shu@hotmail.com>
2018-09-03 16:25:36 -0400

    update SVN and LR

--- SVMKernelized.py
+++ SVMKernelized.py
@@ -1,10 +1,8 @@
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import datasets
-from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import StandardScaler
-from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.svm import SVC
+from matplotlib.colors import ListedColormap
+
 
 def plot_decision_regions(X, y, classifier,
                           test_idx=None, resolution=0.02):
@@ -39,26 +37,21 @@ def plot_decision_regions(X, y, classifier,
                     alpha=1.0, linewidth=1, marker='o',
                     s=55, label='test set')
 
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
 
-iris = datasets.load_iris()
-X = iris.data[:,[2,3]]
-y = iris.target
+plt.scatter(X_xor[y_xor==1, 0], X_xor[y_xor==1, 1],
+            c='b', marker='x', label='1')
+plt.scatter(X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1],
+            c='r', marker='s', label='-1')
+plt.ylim(-3.0)
+plt.legend()
+plt.show()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-
-knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
-
-sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
-X_combined_std = np.vstack((X_train_std, X_test_std))
-y_combined = np.hstack((y_train, y_test))
-
-knn.fit(X_train_std, y_train)
-plot_decision_regions(X_combined_std, y_combined, classifier=knn, test_idx=range(105,150))
-
-plt.xlabel('petal length [standardized]')
-plt.ylabel('petal width [standardized]')
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
 plt.legend(loc='upper left')
 plt.show()
