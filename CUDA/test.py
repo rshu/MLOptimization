9d4c6491183742a56d710d091ba78e2318e8ab7e
Rui Shu <terry.shu@hotmail.com>
2018-11-01 14:47:59 -0400

    update

---
+++ CUDA/test.py
@@ -0,0 +1,28 @@
+import numpy as np
+from timeit import default_timer as timer
+
+
+def vectorAdd(a, b, c):
+    for i in range(a.size):
+        c[i] = a[i] + b[i]
+
+
+def main():
+    N = 32000000
+
+    A = np.ones(N, dtype=np.float32)
+    B = np.ones(N, dtype=np.float32)
+    C = np.zeros(N, dtype=np.float32)
+
+    start = timer()
+    vectorAdd(A, B, C)
+    vectoradd_time = timer() - start
+
+    print("C[:5] = " + str(C[:5]))
+    print("C[-5:] = " + str(C[-5:]))
+
+    print("VectorAdd took %f seconds" % vectoradd_time)
+
+
+if __name__ == '__main__':
+    main()
